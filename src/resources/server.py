c9258c263d1339237bed822c1291eb2304037d68
frostygum <4nthonius.juan5@gmail.com>
2021-04-22 22:33:53 +0700

    update server module

--- src/resources/server.py
+++ src/resources/server.py
@@ -9,45 +9,65 @@ class Server():
         self.ADDR = (self.HOST, self.PORT)
         self.HEADER = 1024
         self.FORMAT = 'utf-8'
-        self.DISCONNECT_MESSAGE = "!DISCONNECT"
-        self.client = []
+        self.DISCONNECT_MESSAGE = '!DISCONNECT'
+        self.client = None
         self.create_socket()
 
     def create_socket(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind(self.ADDR)
 
-    def handle_client(self, conn, addr):
-        print(f"[NEW CONNECTION] {addr} connected.")
+    def handle_reply(self):
+        conn, addr = self.client
+        print('[NEW CONNECTION] {} connected.'.format(addr))
         connected = True
 
         while connected:
-            data = conn.recv(self.HEADER)
-            if data:
-                data = int(data)
-                msg = conn.recv(data).decode(self.FORMAT)
+            try:
+                msg = conn.recv(self.HEADER).decode(self.FORMAT)
                 if msg == self.DISCONNECT_MESSAGE:
                     connected = False
+            except:
+                print('[{}] Disconnected.'.format(addr))
+                self.wait_client()
+                connected = False
+                break
 
-                print(f"[{addr}] {msg}")
-                conn.send(msg.encode(self.FORMAT))
+            print('[{}] {}'.format(addr, msg))
 
         conn.close()
-        self.client.pop()
+    
+    def wait_client(self):
+        conn, addr = self.server.accept()
+        self.create_client(conn, addr)
+
+    def send(self, msg):
+        conn, addr = self.client
+        message = msg.encode(self.FORMAT)
+        conn.send(message)
 
     def create_client(self, conn, addr):
-        self.client.append(addr)
-        thread = threading.Thread(target = self.handle_client, args = (conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        self.client = (conn, addr)
+        reply_thread = threading.Thread(target = self.handle_reply)
+        
+        reply_thread.start()
+        print('[ACTIVE CONNECTIONS] {}'.format(threading.activeCount() - 1))
         
     def start(self):
         self.server.listen()
-        print(f"[LISTENING] Server is listening on {self.HOST}")
+        print('[LISTENING] Server is listening on {}'.format(self.HOST))
     
-        while True:
-            conn, addr = self.server.accept()
-            if (threading.activeCount() - 1) < 2:
-                self.create_client(conn, addr)
-            else:
-                conn.close()
+        self.wait_client()
+
+        # while True:
+        #     reply = input()
+        #     conn, _ = self.client
+        #     conn.send(reply.encode(self.FORMAT))
+
+# if __name__ == '__main__':
+#     server = Server()
+#     try:
+#         server.start()
+#     except KeyboardInterrupt:
+#         print('Interrupted')
+#         sys.exit(0)
