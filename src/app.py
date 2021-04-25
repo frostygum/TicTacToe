5d32800424ac1dd46451dd4705317e86574eea70
frostygum <4nthonius.juan5@gmail.com>
2021-04-25 16:37:46 +0700

    UI Flow Done (#3)

--- src/app.py
+++ src/app.py
@@ -2,10 +2,13 @@
 
 import sys
 from os import environ
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QStackedWidget
 
 from view.main_view import MainView
+from view.game_view import GameView 
 from controller.main_controller import MainController
+from controller.game_controller import GameController
+from model.board import Board
 
 __version__ = '0.1'
 
@@ -18,14 +21,24 @@ def suppress_qt_warnings():
 class App(QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
-        self.main_view = MainView()
-        self.main_controller = MainController(self.main_view)
-        self.main_view.show()
+        views = {}
+        views['main'] = MainView()
+        views['game'] = GameView()
+        self.navigationWidget = QStackedWidget()
+
+        board = Board()
+        self.main_controller = MainController(self.navigationWidget, views)
+        self.game_controller = GameController(self.navigationWidget, views, board)
+
+        self.navigationWidget.setWindowTitle('TicTacToe')
+        self.navigationWidget.setFixedSize(330, 200)
+        self.navigationWidget.addWidget(views['main'])
+        self.navigationWidget.show()
 
 def main():
     app = App(sys.argv)
     exec = app.exec_()
-    app.main_controller.disconnect()
+    # app.main_controller.disconnect()
     sys.exit(exec)
 
 if __name__ == '__main__':
---
+++ src/controller/game_controller.py
@@ -0,0 +1,59 @@
+from functools import partial
+from controller.base_controller import BaseController
+from resources.client import Client
+from model.board import Board
+
+class GameController(BaseController):
+    def __init__(self, navigationWidget, views, board):
+        BaseController.__init__(self, navigationWidget, views)
+        self.board = board
+        self.gameView.createTicTacToeGrid(board)
+        self.connectSignals()
+
+    def connectSignals(self):
+        for location, button in self.gameView.buttons.items():
+            button.clicked.connect(partial(self.btnClicked, location))
+
+    def btnClicked(self, location):
+        self.board.updateLocation('X', location)
+        self.gameView.updateButton('X', location, False)
+        print(self.board.toJSON())
+        if self.checkGameWinner():
+            self.showDialog('Gave Over, Player X Wins', 'Game Over', self.gameOver)
+
+    def checkGameWinner(self):
+        state = self.board.gameState
+        if state['00'] == state['01'] == state['02'] != None:
+            print('Game Over')
+            return True
+        elif state['10'] == state['11'] == state['12'] != None:
+            print('Game Over')
+            return True
+        elif state['20'] == state['21'] == state['22'] != None:
+            print('Game Over')
+            return True
+        elif state['00'] == state['10'] == state['20'] != None:
+            print('Game Over')
+            return True
+        elif state['01'] == state['11'] == state['21'] != None:
+            print('Game Over')
+            return True
+        elif state['02'] == state['12'] == state['22'] != None:
+            print('Game Over')
+            return True
+        elif state['00'] == state['11'] == state['22'] != None:
+            print('Game Over')
+            return True
+        elif state['02'] == state['11'] == state['20'] != None:
+            print('Game Over')
+            return True
+        return False
+
+    def gameOver(self):
+        self.board = Board()
+        for xPos in range(0, self.board.size):
+            for yPos in  range(0, self.board.size):
+                location = '{}{}'.format(xPos, yPos)
+                self.gameView.updateButton(' ', location)
+        self.changeWindow('main')
+        
