379f5e70474993f79bc2c7b85f9f720efa84df68
frostygum <4nthonius.juan5@gmail.com>
2021-04-27 15:12:56 +0700

    restructure base controller

--- src/controller/base_controller.py
+++ src/controller/base_controller.py
@@ -1,27 +1,47 @@
+
+#! Import required PyQt5 modules
 from PyQt5.QtWidgets import QMessageBox
 
 class BaseController:
+    """
+        This class is used as base for created controller
+    """
+
     def __init__(self, navigationWidget, views):
+        """Initiate function"""
+
         self.mainView = views['start']
         self.gameView = views['game']
         self.navigationWidget = navigationWidget
 
     def changeWindow(self, windowPage):
+        """Function to change window with given param"""
+
         if(windowPage == 'start'):
+            #! Goto Main Page
             window = self.mainView
+            #! Reset sizing
             self.navigationWidget.setWindowTitle('TicTacToe')
             self.navigationWidget.setFixedSize(330, 200)
         elif(windowPage == 'game'):
+            #! Goto GGame Page
             window = self.gameView
-            self.navigationWidget.setWindowTitle('Player X Playing')
-            self.navigationWidget.setFixedSize(330, 330)
+            #! Reset sizing
+            self.navigationWidget.setWindowTitle('TicTacToe')
+            self.navigationWidget.setFixedSize(330, 360)
+        
+        #! Create empty status bar
+        window.createStatusBar('')
         self.navigationWidget.addWidget(window)
         self.navigationWidget.setCurrentIndex(self.navigationWidget.currentIndex() + 1)
 
     def showDialog(self, msg, title, callback = None):
+        """Function to show Alert Dialog"""
+
         msgBox = QMessageBox()
         msgBox.setText(msg)
-        msgBox.setWindowTitle('Alert')
+        msgBox.setWindowTitle(title)
         msgBox.exec()
+        #! Run any given callback after user close the dialog
         if callback != None:
             callback()
